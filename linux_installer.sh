a245c91ab54d171cccb605d0b1fe58670ecfee9d
Igor Machado <igor.machado@gmail.com>
2019-04-13 18:03:40 -0300

    adding NUC script

--- linux_installer.sh
+++ linux_installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Welcome to NUC installer (Unified Compilers for Neo Blockchain)"
+echo "*********************************************************************"
+echo "Welcome to NUC linux installer (Unified Compilers for Neo Blockchain)"
+echo "*********************************************************************"
 echo "This script is currently focused on debian-based/ubuntu systems, so feel free to test if this works on other systems"
 
 if [ "$EUID" -ne 0 ]
@@ -75,14 +77,22 @@ echo "Will replace Framework support for netcoreapp2.0"
 sed -i -e 's/netstandard1.6;net40/netstandard2.0;netcoreapp2.0/g' /opt/nuc/neon/neo-devpack-dotnet/Neo.SmartContract.Framework/Neo.SmartContract.Framework.csproj
 (cd /opt/nuc/neon/neo-devpack-dotnet/Neo.SmartContract.Framework && dotnet restore && \
         dotnet build -c Release && dotnet publish -f netcoreapp2.0 -c Release && \
-        mkdir -p /opt/nuc/neon/lib-devpack-master && \
-        mv /opt/nuc/neon/neo-devpack-dotnet/Neo.SmartContract.Framework/bin/Release/netcoreapp2.0/publish/* /opt/nuc/neon/lib-devpack-master)
+        mkdir -p /opt/nuc/neon/lib-csharp-devpack-master && \
+        mv /opt/nuc/neon/neo-devpack-dotnet/Neo.SmartContract.Framework/bin/Release/netcoreapp2.0/publish/* /opt/nuc/neon/lib-csharp-devpack-master)
 
-echo "========================================="
-echo "Please add NUC to path: /opt/nuc/bin     "
-echo "edit: ~/.profile                         "
+echo "=============================="
+echo "Installing NUC to /opt/nuc/bin"
+echo "=============================="
+
+mkdir -p /opt/nuc/bin
+cp nuc.sh /opt/nuc/bin/nuc
+chmod +x /opt/nuc/bin/nuc
+
+echo "=========================================="
+echo "Please add NUC to path: /opt/nuc/bin      "
+echo "edit: ~/.profile                          "
 echo "add line: export PATH=\$PATH:/opt/nuc/bin "
-echo "close editor and run: source ~/.profile  "
-echo "========================================="
+echo "close editor and run: source ~/.profile   "
+echo "=========================================="
 
 echo "Finished!"
---
+++ nuc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Welcome to NUC (Unified Compilers for Neo Blockchain)"
+dotnet /opt/nuc/neon/bin-neon-master/neon.dll --version
